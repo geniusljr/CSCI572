3a4767da9d2595b5dfd044a3a50d10068624f9e7
LiJiarui <lijiarui@LiJiaruis-MacBook-Pro.local>
2014-09-30 21:21:20 -0700

    modify the code of jsonTableContentHandler Integrate the two main functions into a new main function.

--- src/Assignment1/JSONTableContentHandler.java
+++ src/Assignment1/JSONTableContentHandler.java
@@ -2,133 +2,101 @@ package Assignment1;
 
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.tika.metadata.Metadata;
 import org.apache.tika.sax.ToTextContentHandler;
-import org.apache.tika.sax.XHTMLContentHandler;
-import org.apache.tika.sax.xpath.Matcher;
-import org.apache.tika.sax.xpath.XPathParser;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-public class JSONTableContentHandler extends ToTextContentHandler{
-	
-	JsonArray jArr;
-	ArrayList<String> names = new ArrayList<String>();
-	ArrayList<String> values = new ArrayList<String>();
-	Boolean isHead = false;
-	Boolean isData = false;
-	
+public class JSONTableContentHandler extends ToTextContentHandler {
+
+    private JsonArray         jsonArray;
+    private ArrayList<String> names       = new ArrayList<String>();
+    private ArrayList<String> values      = new ArrayList<String>();
+    private boolean           headStarted = false;
+    private boolean           dataStarted = false;
+
     /**
      * Creates an JSON serializer that writes to the given byte stream
      *
-     * @param stream output stream
+     * @param stream
+     *            output stream
      */
-    public JSONTableContentHandler(OutputStream stream){
+    public JSONTableContentHandler(OutputStream stream) {
         super(stream);
     }
+
     public JSONTableContentHandler() {
         super();
     }
-    
+
     /**
      * Create Json Object
      */
     @Override
-    public void startElement(
-            String uri, String localName, String qName, Attributes atts)
+    public void startElement(String uri, String localName, String qName, Attributes atts)
             throws SAXException {
-    	switch (localName){
-    		case "table":
-    			jArr = new JsonArray();
-    			break;
-    		case "tr":
-    			isHead = false;
-    			isData = false;
-    			break;
-    		case "th":
-    			isHead = true;
-    			break;
-    		case "td":
-    			isData = true;
-    			break;
-    		default:
-    			isHead = false;
-    			isData = false;
-    			break;
-    	}
+        if (localName.equals("table")) {
+            jsonArray = new JsonArray();
+        } else if (localName.equals("th")) {
+            headStarted = true;
+        } else if (localName.equals("td")) {
+            dataStarted = true;
+        }
     }
-    
 
     @Override
-    public void endElement(String uri, String localName, String qName)
-            throws SAXException {
-    	switch (localName){
-			case "table":
-				// end of the table
-				write(jArr.toString());
-				break;
-			case "tr":
-				// end of a line (th or td)
-				// write(names.toString());
-				// if end of td: 1. convert all values to curObj; 2. add to jArr
-				if(values.size()>0){
-					JsonObject curObj = Convert2Json(names, values);
-					jArr.add(curObj);
-					values.clear();
-				}else{
-					isHead = false;
-				}
-				break;
-			case "th":
-				// end of a header
-				break;
-			case "td":
-				// end of a data
-				break;
-			default:
-				break;
-    	}
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (localName.equals("table")) {
+            write(jsonArray.toString());
+        } else if (localName.equals("tr")) {
+            if (values.size() > 0) {
+                JsonObject curObj = Convert2Json(names, values);
+                jsonArray.add(curObj);
+                values.clear();
+            }
+        }
     }
-    
+
     @Override
-    public void characters(char[] ch, int start, int length)
-            throws SAXException {
-    	// get current data
-    	String curStr = String.valueOf(ch, start, length);
-    	if(isHead){
-    		names.add(curStr);
-    		isHead = false;
-    	}else if(isData){
-    		values.add(curStr);
-    		isData = false;
-    	}
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        // get current data
+        String curStr = String.valueOf(ch, start, length);
+        // For tags like <td \>, endElement will be called before characters,
+        // so closing the headStarted and dataStarted should be finished within
+        // this function instead of endElement.
+        if (headStarted) {
+            names.add(curStr);
+            headStarted = false;
+        } else if (dataStarted) {
+            values.add(curStr);
+            dataStarted = false;
+        }
     }
-    
-    /**
-     * Writes the given string of character as-is.
-     *
-     * @param string string of character to be written
-     * @throws SAXException if the character string could not be written
-     */
+
     protected void write(char ch) throws SAXException {
         super.characters(new char[] { ch }, 0, 1);
     }
+
+    /**
+     * Writes the given string of character as-is.
+     *
+     * @param string
+     *            string of character to be written
+     * @throws SAXException
+     *             if the character string could not be written
+     */
     protected void write(String string) throws SAXException {
         super.characters(string.toCharArray(), 0, string.length());
     }
-    
-    protected JsonObject Convert2Json(ArrayList<String> names, ArrayList<String> values){
-    	JsonObject curObj = new JsonObject();
-    	for(int i=0; i<names.size(); i++){
-    		curObj.addProperty(names.get(i), values.get(i));
-    	}
-    	return curObj;
+
+    protected JsonObject Convert2Json(ArrayList<String> names, ArrayList<String> values) {
+        JsonObject curObj = new JsonObject();
+        for (int i = 0; i < names.size(); i++) {
+            curObj.addProperty(names.get(i), values.get(i));
+        }
+        return curObj;
     }
 }
--- src/Assignment1/Main.java
+++ src/Assignment1/Main.java
@@ -11,37 +11,34 @@ import java.io.OutputStream;
 
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.sax.ToXMLContentHandler;
+import org.apache.tika.sax.XHTMLContentHandler;
+import org.apache.tika.sax.xpath.Matcher;
+import org.apache.tika.sax.xpath.MatchingContentHandler;
+import org.apache.tika.sax.xpath.XPathParser;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        Parser parser = new TSVParser();
+    public static void TSV2XHTML(String inputPath, String outputPath) throws IOException {
 
-        String inputPath = "/Users/lijiarui/Documents/workspace/CSCI572/dataset/computrabajo-ar-20121106.tsv";
-        String outputPath = "/Users/lijiarui/Documents/workspace/CSCI572/dataset/a.xml";
         InputStream inputStream = null;
         OutputStream outputStream = null;
 
         try {
-
             inputStream = new BufferedInputStream(new FileInputStream(new File(inputPath)));
             outputStream = new BufferedOutputStream(new FileOutputStream(new File(outputPath)));
-
             ContentHandler handler = new ToXMLContentHandler(outputStream, "utf-8");
             Metadata metadata = new Metadata();
-
+            Parser parser = new TSVParser();
             parser.parse(inputStream, handler, metadata, new ParseContext());
-
         } catch (SAXException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (TikaException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } finally {
             if (inputStream != null) {
@@ -52,4 +49,49 @@ public class Main {
             }
         }
     }
+
+    public static void XHTML2JSON(String inputPath, String outputPath) throws IOException {
+        Parser parser = new AutoDetectParser();
+
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = new BufferedInputStream(new FileInputStream(new File(inputPath)));
+            outputStream = new BufferedOutputStream(new FileOutputStream(new File(outputPath)));
+            // TODO Implement the JSONTableContentHandler
+            XPathParser xhtmlParser = new XPathParser("xhtml", XHTMLContentHandler.XHTML);
+            Matcher divContentMatcher = xhtmlParser
+                    .parse("/xhtml:html/xhtml:body/descendant::node()");
+            ContentHandler handler = new MatchingContentHandler(new JSONTableContentHandler(
+                    outputStream), divContentMatcher);
+            Metadata metadata = new Metadata();
+            parser.parse(inputStream, handler, metadata, new ParseContext());
+            
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (TikaException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        String tsvPath = "dataset/computrabajo-ar-20121106.tsv";
+        String xhtmlPath = "dataset/1.html";
+        String jsonPath = "dataset/1.json";
+        try {
+            TSV2XHTML(tsvPath, xhtmlPath);
+            XHTML2JSON(xhtmlPath, jsonPath);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 }
